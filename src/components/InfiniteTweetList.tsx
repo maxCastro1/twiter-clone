bce0e0795f2fc6ddd9fbd4022aa6c41675928d53
maxCastro1 <maxmizlg512@gmail.com>
2023-06-08 08:03:28 +0200

    add changes

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -31,8 +31,12 @@ const InfiniteTweetList = ({
     fetchNewTweets,
     hasMore = false,} : InfiniteTweetListProps) => {
         if (isLoading) return <LoadingSpinner/>;
-        if (isError) return <h1>Error...</h1>;
-      
+
+        if (isError) {
+          console.log(isError)
+          return <h1 className="flex m-4 text-center">Something went wrong, please try again.</h1>;
+        }
+        
         if (tweets == null || tweets.length === 0) {
           return (
             <h2 className="my-4 text-center text-2xl text-gray-500">No Tweets</h2>
